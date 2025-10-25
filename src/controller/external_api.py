694d2714406ef4e19c24d57e366fe18ec9e13253
MalinkyZubr <ahumanbeing189@gmail.com>
2025-10-25 18:47:38 -0400

    finished implementing all but 2 endpoints, will work on refactorign and reorganization, then testing

--- src/controller/external_api.py
+++ src/controller/external_api.py
@@ -92,7 +92,8 @@ VALIDATION_ERROR_MESSAGE_LOOKUP: dict[tuple[str, str], str] = {
     ("DELETE", "/artifacts/*/*"):"There is missing field(s) in the artifact_type or artifact_id or invalid",
     ("PUT", "/artifact/*/*"):"There is missing field(s) in the artifact_type or artifact_id or it is formed improperly, or is invalid.",
     ("GET", "/artifact/model/*/rate"):"There is missing field(s) in the artifact_id or it is formed improperly, or is invalid.",
-    ("POST", "/artifact/*"):"There is missing field(s) in the artifact_data or it is formed improperly (must include a single url)."
+    ("POST", "/artifact/*"):"There is missing field(s) in the artifact_data or it is formed improperly (must include a single url).",
+    ("GET", "/artifact/*/*/cost"):"There is missing field(s) in the artifact_type or artifact_id or it is formed improperly, or is invalid."
 }
 
 def get_validation_error_message(key: tuple[str, str]) -> str:
@@ -155,6 +156,14 @@ class ArtifactAccessor:
     def register_artifact(self, artifact_type: ArtifactType, body: ArtifactData) -> tuple[RegisterArtifactEnum, Artifact]:
         raise NotImplementedError()
 
+    @validate_call
+    def get_artifact_by_name(self, name: ArtifactName) -> tuple[GetArtifactEnum, list[ArtifactMetadata]]:
+        raise NotImplementedError()
+
+    @validate_call
+    def get_artifact_by_regex(self, regex_exp: ArtifactRegEx) -> tuple[GetArtifactEnum, list[ArtifactMetadata]]:
+        raise NotImplementedError()
+
 
 async def artifact_accessor() -> ArtifactAccessor:
     return ArtifactAccessor()
@@ -181,6 +190,48 @@ async def get_artifacts(
             raise RequestValidationError(errors=["internal"])
         case return_code.TOO_MANY_ARTIFACTS:
             raise HTTPException(status_code=return_code.value, detail="Too many artifacts returned.")
+
+
+@access_level(AccessLevel.NO_AUTHENTICATION)
+@app.post("/artifact/byName/{name}", status_code = status.HTTP_200_OK)
+async def get_artifacts_by_name(
+        name: ArtifactName,
+        accessor: Annotated[ArtifactAccessor, Depends(artifact_accessor)],
+        x_authorization: str = Depends(VerifyAuth())
+) -> List[ArtifactMetadata] | None:
+    return_code: GetArtifactEnum
+    return_content: List[ArtifactMetadata]
+
+    return_code, return_content = accessor.get_artifact_by_name(name)
+
+    match return_code:
+        case return_code.SUCCESS:
+            return return_content
+        case return_code.INVALID_REQUEST:
+            raise RequestValidationError(errors=["internal"])
+        case return_code.DOES_NOT_EXIST:
+            raise HTTPException(status_code=return_code.value, detail="No such artifact.")
+
+
+@access_level(AccessLevel.NO_AUTHENTICATION)
+@app.post("/artifact/byRegEx", status_code = status.HTTP_200_OK)
+async def get_artifacts_by_name(
+        regex: ArtifactRegEx,
+        accessor: Annotated[ArtifactAccessor, Depends(artifact_accessor)],
+        x_authorization: str = Depends(VerifyAuth())
+) -> List[ArtifactMetadata] | None:
+    return_code: GetArtifactEnum
+    return_content: List[ArtifactMetadata]
+
+    return_code, return_content = accessor.get_artifact_by_regex(regex)
+
+    match return_code:
+        case return_code.SUCCESS:
+            return return_content
+        case return_code.INVALID_REQUEST:
+            raise RequestValidationError(errors=["internal"])
+        case return_code.DOES_NOT_EXIST:
+            raise HTTPException(status_code=return_code.value, detail="No artifact found under this regex.")
 
 
 async def reset_registry():
@@ -300,7 +351,7 @@ class ModelRater:
         raise NotImplementedError()
 
 
-async def artifact_rater() -> ModelRater:
+async def model_rater() -> ModelRater:
     return ModelRater()
 
 @access_level(AccessLevel.USER_AUTHENTICATION)
@@ -308,9 +359,9 @@ async def artifact_rater() -> ModelRater:
 async def rate_model(
         id: ArtifactID,
         response: Response,
-        rater: Annotated[ModelRater, Depends(artifact_rater)],
+        rater: Annotated[ModelRater, Depends(model_rater)],
         x_authorization: str = Depends(VerifyAuth()),
-):
+) -> ModelRating | None:
     return_code: ModelRaterEnum
     return_content: ModelRating
 
@@ -325,3 +376,76 @@ async def rate_model(
             raise HTTPException(status_code=return_code.value, detail="Artifact does not exist.")
         case return_code.INTERNAL_ERROR:
             raise HTTPException(status_code=return_code.value, detail="The artifact rating system encountered an error while computing at least one metric.")
+
+
+class ArtifactCostAnalyzer:
+    @validate_call
+    async def get_artifact_cost(self, artifact_type: ArtifactType, id: ArtifactID) -> tuple[ModelRaterEnum, ArtifactCost]:
+        raise NotImplementedError()
+
+
+async def get_artifact_cost() -> ArtifactCostAnalyzer:
+    return ArtifactCostAnalyzer()
+
+@access_level(AccessLevel.USER_AUTHENTICATION)
+@app.get("/artifact/{artifact_type}/{id}/cost", status_code=status.HTTP_200_OK)
+async def rate_model(
+        id: ArtifactID,
+        artifact_type: ArtifactType,
+        response: Response,
+        cost_analyzer: Annotated[ArtifactCostAnalyzer, Depends(get_artifact_cost)],
+        x_authorization: str = Depends(VerifyAuth()),
+) -> ArtifactCost | None:
+    return_code: ModelRaterEnum
+    return_content: ArtifactCost
+
+    return_code, return_content = await cost_analyzer.get_artifact_cost(artifact_type, id)
+
+    match return_code:
+        case return_code.SUCCESS:
+            return return_content
+        case return_code.INVALID_REQUEST:
+            raise RequestValidationError(errors=["internal"])
+        case return_code.NOT_FOUND:
+            raise HTTPException(status_code=return_code.value, detail="Artifact does not exist.")
+        case return_code.INTERNAL_ERROR:
+            raise HTTPException(status_code=return_code.value, detail="The artifact cost calculator encountered an error.")
+
+
+class LineageEnum(Enum):
+    SUCCESS = 200
+    MALFORMED = 400
+    DOES_NOT_EXIST = 404
+class LineageGraphAnalyzer:
+    @validate_call
+    async def get_lineage_graph(self, id: ArtifactID) -> tuple[LineageEnum, ArtifactLineageGraph]:
+        raise NotImplementedError()
+
+async def get_artifact_lineage_graph() -> LineageGraphAnalyzer:
+    return LineageGraphAnalyzer()
+
+
+@access_level(AccessLevel.USER_AUTHENTICATION)
+@app.get("/artifact/model/{id}/lineage", status_code=status.HTTP_200_OK)
+async def get_model_lineage(
+        id: ArtifactID,
+        response: Response,
+        lineage_analyzer: Annotated[LineageGraphAnalyzer, Depends(get_artifact_lineage_graph)],
+        x_authorization: str = Depends(VerifyAuth()),
+) -> ArtifactLineageGraph | None:
+    return_code: LineageEnum
+    return_content: ArtifactLineageGraph
+
+    return_code, return_content = await lineage_analyzer.get_lineage_graph(id)
+
+    match return_code:
+        case return_code.SUCCESS:
+            return return_content
+        case return_code.MALFORMED:
+            raise RequestValidationError(errors=["internal"])
+        case return_code.DOES_NOT_EXIST:
+            raise HTTPException(status_code=return_code.value, detail="Artifact does not exist.")
+
+
+# TODO: LICENSE CHECK!
+# TODO: TRACKS ENDPOINT!
